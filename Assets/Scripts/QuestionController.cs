7a747307bc977df7833ded19c06c0855b81aa38f
Xblivior <1001701@student.sae.edu.au>
2016-06-18 11:58:12 +0800

    Started question script

---
+++ Assets/Scripts/QuestionController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuestionController : MonoBehaviour 
+{
+	int a;
+	int b;
+	int c;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void RandomType()
+	{
+		int rNum = Random.Range (0,4);
+
+		if (rNum = 0)
+		{
+			Addition();
+		}
+
+		else if (rNum = 1)
+		{
+			Subtraction();
+		}
+
+		else if (rNum = 2)
+		{
+			Multiplication();
+		}
+
+		else (rNum = 3)
+		{
+			Division();
+		}
+
+	}
+
+	void Addition()
+	{
+		
+	}
+
+	void Subtraction()
+	{
+
+	}
+
+	void Multiplication()
+	{
+
+	}
+
+	void Division()
+	{
+
+	}
+}
