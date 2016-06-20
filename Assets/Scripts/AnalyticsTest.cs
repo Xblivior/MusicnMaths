46173151729ae1dbbf576db019db5e365917a946
Xblivior <1001701@student.sae.edu.au>
2016-06-20 13:52:55 +0800

    Analytics Test

---
+++ Assets/Scripts/AnalyticsTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Xml;
+
+public class AnalyticsTest : MonoBehaviour 
+{
+	//IDictionary<string, object> startData = new Dictionary<string, object>();
+	Dictionary<string, object> gameStart = new Dictionary<string, object>();
+	// Use this for initialization
+	void Start () 
+	{
+		gameStart.Add ("DeviceID", SystemInfo.deviceUniqueIdentifier);
+		Analytics.CustomEvent("GameStart", gameStart);
+		Analytics.Transaction ("Song13",(decimal) 1.55, "EUR");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
