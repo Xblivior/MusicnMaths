6fac2827af167bed5809d5f97327e4c41a090fa5
Xblivior <1001701@student.sae.edu.au>
2016-06-26 22:30:02 +0800

    Colour change works........kinda

--- Assets/TutorialStuff/ColourChange.cs
+++ Assets/TutorialStuff/ColourChange.cs
@@ -4,17 +4,47 @@ using System.Collections;
 public class ColourChange : MonoBehaviour 
 {
 	public Material mat;
-	public Color lerpedColour;
+	float currentR;
+	float currentG;
+	float currentB;
+
+	float rdmR;
+	float rdmG;
+	float rdmB;
+
+	float timer = 2.5f;
+
 	// Use this for initialization
 	void Start () 
 	{
-		
+		currentR = mat.color.r;
+		currentG = mat.color.g;
+		currentB = mat.color.b;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		mat.color = lerpedColour;
-		lerpedColour = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+		timer = timer - Time.deltaTime;
+		if (timer <= 0.0f)
+		{
+			ChangingColour();
+			timer = 2.5f;
+		}
+			
+	}
+
+	void ChangingColour()
+	{
+		rdmR = Random.Range(0.01f, 1.00f); 
+		rdmG = Random.Range(0.01f, 1.00f);
+		rdmB = Random.Range(0.01f, 1.00f);
+
+		currentR = Mathf.Lerp(currentR, rdmR, Time.deltaTime * 10);
+		currentG = Mathf.Lerp(currentG, rdmG, Time.deltaTime * 10);
+		currentB = Mathf.Lerp(currentB, rdmB, Time.deltaTime * 10);
+
+		mat.color = new Color(currentR, currentG, currentB);
+
 	}
 }
--- Assets/TutorialStuff/Spectrum.cs
+++ Assets/TutorialStuff/Spectrum.cs
@@ -9,19 +9,9 @@ public class Spectrum : MonoBehaviour
 
 	public GameObject[] cubeArray;
 
-	public Material mat; 
-	float r;
-	float g;
-	float b;
-
-
 	// Use this for initialization
 	void Start () 
 	{
-		r = mat.color.r;
-		g = mat.color.g;
-		b = mat.color.b;
-
 		for (int i = 0; i < numberOfObjects; i++)
 		{
 			float angle = i * Mathf.PI * 2 / numberOfObjects;
@@ -43,20 +33,6 @@ public class Spectrum : MonoBehaviour
 			cubeArray[i].transform.localScale = previousScale;
 		}
 
-		//ColourChange();
-
-
-	}
-
-	void ColourChange()
-	{
-//		float rdmR = Random.Range((1/100),(101/100)); 
-//		float rdmG = Random.Range((1/100),(101/100));
-//		float rdmB = Random.Range((1/100),(101/100));
-
-//		mat.color.r = Mathf.Lerp (r, rdmR, Time.deltaTime * 30);
-
-//		mat.color = new Color(rdmR, rdmG, rdmB);
 	}
 		
 }
