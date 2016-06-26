25ad17c7138debd2dbf025b4e5285bfcc83c18f6
Xblivior <1001701@student.sae.edu.au>
2016-06-26 21:59:22 +0800

    Trying to Change colour over time

---
+++ Assets/TutorialStuff/ColourChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColourChange : MonoBehaviour 
+{
+	public Material mat;
+	public Color lerpedColour;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		mat.color = lerpedColour;
+		lerpedColour = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+	}
+}
--- Assets/TutorialStuff/Spectrum.cs
+++ Assets/TutorialStuff/Spectrum.cs
@@ -9,9 +9,19 @@ public class Spectrum : MonoBehaviour
 
 	public GameObject[] cubeArray;
 
+	public Material mat; 
+	float r;
+	float g;
+	float b;
+
+
 	// Use this for initialization
 	void Start () 
 	{
+		r = mat.color.r;
+		g = mat.color.g;
+		b = mat.color.b;
+
 		for (int i = 0; i < numberOfObjects; i++)
 		{
 			float angle = i * Mathf.PI * 2 / numberOfObjects;
@@ -29,8 +39,30 @@ public class Spectrum : MonoBehaviour
 		for (int i = 0; i < numberOfObjects; i++)
 		{
 			Vector3 previousScale = cubeArray[i].transform.localScale;
-			previousScale.y = spectrum[i] * 20;
+			previousScale.y = Mathf.Lerp(previousScale.y, spectrum[i] * 30, Time.deltaTime * 30);
 			cubeArray[i].transform.localScale = previousScale;
 		}
+
+		//ColourChange();
+
+
 	}
+
+	void ColourChange()
+	{
+//		float rdmR = Random.Range((1/100),(101/100)); 
+//		float rdmG = Random.Range((1/100),(101/100));
+//		float rdmB = Random.Range((1/100),(101/100));
+
+//		mat.color.r = Mathf.Lerp (r, rdmR, Time.deltaTime * 30);
+
+//		mat.color = new Color(rdmR, rdmG, rdmB);
+	}
+		
 }
+
+
+
+
+//tutorial by The Developer Guy 
+//https://www.youtube.com/watch?v=ELLANEFw5B8  
