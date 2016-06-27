a3a55bcc5fce2e6bd030e33b7b80620d00646bc9
Xblivior <1001701@student.sae.edu.au>
2016-06-27 16:28:52 +0800

    line renderer implemented

--- Assets/TutorialStuff/Spectrum.cs
+++ Assets/TutorialStuff/Spectrum.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class Spectrum : MonoBehaviour 
 {
+	//references for outer cube ring
 	public GameObject cubes;
 	public int numberOfObjects = 20;
 	public float radius = 5f;
 
+	//references for inner cube ring
 	public GameObject cubesT;
 	public int numberOfObjectsT = 20;
 	public float radiusT = 5f;
 
+	//cube arrays
 	public GameObject[] cubeArray;
 	public GameObject[] cubeArrayT;
 
+	//camera pivot
 	public GameObject cameraPivot;
 
+	//cube parent stuff
 	public GameObject cubeParent;
 	public GameObject cubeParentLookAt;
 
@@ -23,7 +28,7 @@ public class Spectrum : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		
+		//instantiating the outer cubes
 		for (int i = 0; i < numberOfObjects; i++)
 		{
 			float angle = i * Mathf.PI * 2 / numberOfObjects;
@@ -31,6 +36,7 @@ public class Spectrum : MonoBehaviour
 			Instantiate(cubes, pos, Quaternion.identity);
 		}
 
+		//instantiating the inner cubes + adding them to a parent
 		for (int iT = 0; iT < numberOfObjectsT; iT++)
 		{
 			float angleT = iT * Mathf.PI * 2 / numberOfObjectsT;
@@ -40,8 +46,11 @@ public class Spectrum : MonoBehaviour
 
 		}
 
+		//get the cubeparent to look at cubeparentlookat to make the second ring of cubes turn around
 		Vector3 parentLookAt = cubeParentLookAt.transform.position;
 		cubeParent.transform.LookAt (parentLookAt);
+
+		//add the cubes to the arrays
 		cubeArray = GameObject.FindGameObjectsWithTag("Cube");
 		cubeArrayT = GameObject.FindGameObjectsWithTag("CubeT");
 	}
@@ -52,18 +61,25 @@ public class Spectrum : MonoBehaviour
 		float[] spectrum = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
 		for (int i = 0; i < numberOfObjects; i++)
 		{
-			Vector3 previousScale = cubeArray[i].transform.localScale;
-			previousScale.z = Mathf.Lerp(previousScale.z, spectrum[i] * 30, Time.deltaTime * 30);
-			cubeArray[i].transform.localScale = previousScale;
+			Vector3 previousPosition = cubeArray[i].transform.position;
+
+			//                                                   (how far out of the middle they spawn)                                         (how much they jump to music)
+			previousPosition = Vector3.Lerp(previousPosition, (cameraPivot.transform.position + -cubeArray[i].transform.forward * 10) + -cubeArray[i].transform.forward * spectrum[i] * 20, Time.deltaTime * 30);
+			cubeArray[i].transform.position = previousPosition;
+			GetComponent<LineRenderer> ().SetPosition (i, previousPosition);
+			if (i == cubeArray.Length - 1)
+			{
+				GetComponent<LineRenderer> ().SetPosition (i + 1, cubeArray[0].transform.position);
+			}
 		}
 
-		float[] spectrumT = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
-		for (int iT = 0; iT < numberOfObjectsT; iT++)
-		{
-			Vector3 previousScaleT = cubeArrayT[iT].transform.localScale;
-			previousScaleT.z = Mathf.Lerp(previousScaleT.z, spectrumT[iT] * 30, Time.deltaTime * 30);
-			cubeArrayT[iT].transform.localScale = previousScaleT;
-		}
+//		float[] spectrumT = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
+//		for (int iT = 0; iT < numberOfObjectsT; iT++)
+//		{
+//			Vector3 previousScaleT = cubeArrayT[iT].transform.localScale;
+//			previousScaleT.z = Mathf.Lerp(previousScaleT.z, spectrumT[iT] * 30, Time.deltaTime * 30);
+//			cubeArrayT[iT].transform.localScale = previousScaleT;
+//		}
 
 		//get camera to rotate around pivote for effect
 		cameraPivot.transform.Rotate (0, 0.2f, 0);
