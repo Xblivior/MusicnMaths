4aa87013fa151791894df1e109158f81bda24160
Xblivior <1001701@student.sae.edu.au>
2016-07-15 11:57:55 +0800

    Addition Tute alpha is done
    Added UI and TuteController

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialController : MonoBehaviour 
+{
+	public Canvas mainCanvas;
+	public Canvas adCanvas;
+	public Canvas subCanvas;
+	public Canvas multiCanvas;
+	public Canvas divCanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+		adCanvas.enabled = false;
+		subCanvas.enabled = false;
+		multiCanvas.enabled = false;
+		divCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void AdditionTute()
+	{
+		//make addition canvas active
+		adCanvas.enabled = true;
+
+		//make other canvases false
+		subCanvas.enabled = false;
+		multiCanvas.enabled = false;
+		divCanvas.enabled = false;
+	}
+
+	public void SubtractionTute()
+	{
+		//make subtraction canvas active
+		subCanvas.enabled = true;
+
+		//make other canvases false
+		adCanvas.enabled = false;
+		multiCanvas.enabled = false;
+		divCanvas.enabled = false;
+	}
+
+	public void MultiplicationTute()
+	{
+		//make mulitplication tute active
+		multiCanvas.enabled = true;
+
+		//make other canvases false
+		adCanvas.enabled = false;
+		subCanvas.enabled = false;
+		divCanvas.enabled = false;
+	}
+
+	public void DivisionTute()
+	{
+		//make division tute active
+		divCanvas.enabled = true;
+
+		//make other canvases false
+		adCanvas.enabled = false;
+		subCanvas.enabled = false;
+		multiCanvas.enabled = false;
+	}
+}
